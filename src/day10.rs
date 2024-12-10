3ecd9490e8bd475522823b39dcf81c68f80de545
Didier Prophete <didier.prophete@gmail.com>
2024-12-09 22:36:01 -0800

    day10-part2 too easy

--- src/day10.rs
+++ src/day10.rs
@@ -32,14 +32,13 @@ impl Matrix<i32> {
         res
     }
 
-    fn nb_trails_for_start(&self, start: V2) -> usize {
+    fn nb_trails_for_start_p1(&self, start: V2) -> usize {
         let mut nb_trails = 0;
         let mut to_explore = vec![start];
         let mut matrix = self.clone();
         while !to_explore.clone().is_empty() {
             let pos = to_explore.pop().unwrap();
             let nxs = matrix.get_nxs(&pos);
-            // check if we found the end
             for nx in nxs {
                 if matrix.get(&nx).unwrap() == 9 {
                     matrix.set(&nx, -1);
@@ -53,16 +52,19 @@ impl Matrix<i32> {
     }
 }
 
-fn p1(input: &str) -> usize {
-    let convert = |c: char| match c {
+fn convert(c: char) -> i32 {
+    match c {
         '.' => -1,
         _ => c.to_digit(10).unwrap() as i32,
-    };
+    }
+}
+
+fn p1(input: &str) -> usize {
     let matrix = Matrix::from_str(input, convert);
 
     let mut nb_trails = 0;
     for start in &matrix.get_starts() {
-        nb_trails += matrix.nb_trails_for_start(*start);
+        nb_trails += matrix.nb_trails_for_start_p1(*start);
     }
     nb_trails
 }
@@ -71,19 +73,51 @@ fn p1(input: &str) -> usize {
 // p2
 //--------------------------------------------------------------------------------
 
+impl Matrix<i32> {
+    fn nb_trails_for_start_p2(&self, start: V2) -> usize {
+        let mut nb_trails = 0;
+        let mut to_explore = vec![start];
+        while !to_explore.clone().is_empty() {
+            let pos = to_explore.pop().unwrap();
+            let nxs = self.get_nxs(&pos);
+            for nx in nxs {
+                if self.get(&nx).unwrap() == 9 {
+                    nb_trails += 1;
+                } else {
+                    to_explore.push(nx);
+                }
+            }
+        }
+        nb_trails
+    }
+}
+
+fn p2(input: &str) -> usize {
+    let matrix = Matrix::from_str(input, convert);
+
+    let mut nb_trails = 0;
+    for start in &matrix.get_starts() {
+        nb_trails += matrix.nb_trails_for_start_p2(*start);
+    }
+    nb_trails
+}
+
 //--------------------------------------------------------------------------------
 // main
 //--------------------------------------------------------------------------------
 
 pub fn run() {
-    time_it(p1, "data/10_sample1.txt");
-    time_it(p1, "data/10_sample2.txt");
-    time_it(p1, "data/10_sample3.txt");
-    time_it(p1, "data/10_sample4.txt");
-    time_it(p1, "data/10_sample5.txt");
+    time_it(p1, "data/10_sample1a.txt");
+    time_it(p1, "data/10_sample1b.txt");
+    time_it(p1, "data/10_sample1c.txt");
+    time_it(p1, "data/10_sample1d.txt");
+    time_it(p1, "data/10_sample1e.txt");
     time_it(p1, "data/10_input.txt");
-    // time_it(p2, "data/10_sample.txt");
-    // time_it(p2, "data/10_input.txt");
+    time_it(p2, "data/10_sample2a.txt");
+    time_it(p2, "data/10_sample2b.txt");
+    time_it(p2, "data/10_sample2c.txt");
+    time_it(p2, "data/10_sample1e.txt");
+    time_it(p2, "data/10_input.txt");
 }
 
 #[cfg(test)]
@@ -92,10 +126,14 @@ mod tests {
 
     #[test]
     fn test() {
-        assert_eq!(run_it(p1, "data/10_sample1.txt"), 1);
-        assert_eq!(run_it(p1, "data/10_sample2.txt"), 2);
-        assert_eq!(run_it(p1, "data/10_sample3.txt"), 4);
-        assert_eq!(run_it(p1, "data/10_sample4.txt"), 3);
-        assert_eq!(run_it(p1, "data/10_sample5.txt"), 36);
+        assert_eq!(run_it(p1, "data/10_sample1a.txt"), 1);
+        assert_eq!(run_it(p1, "data/10_sample1b.txt"), 2);
+        assert_eq!(run_it(p1, "data/10_sample1c.txt"), 4);
+        assert_eq!(run_it(p1, "data/10_sample1d.txt"), 3);
+        assert_eq!(run_it(p1, "data/10_sample1e.txt"), 36);
+        assert_eq!(run_it(p2, "data/10_sample2a.txt"), 3);
+        assert_eq!(run_it(p2, "data/10_sample2b.txt"), 13);
+        assert_eq!(run_it(p2, "data/10_sample2c.txt"), 227);
+        assert_eq!(run_it(p2, "data/10_sample1e.txt"), 81);
     }
 }
