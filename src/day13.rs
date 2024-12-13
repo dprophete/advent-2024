0c12c65501425c0e5b4183a5002e6c8bf96c5010
Didier Prophete <didier.prophete@gmail.com>
2024-12-12 22:02:22 -0800

    day13 - part1

--- src/day13.rs
+++ src/day13.rs
@@ -6,15 +6,15 @@ use crate::utils::*;
 
 #[derive(Debug, Clone, Copy, Hash, Eq, PartialEq)]
 struct Machine {
-    btn_a: V2,
-    btn_b: V2,
-    prize: V2,
+    btn_a: (i32, i32),
+    btn_b: (i32, i32),
+    prize: (i32, i32),
 }
 
-fn parse_dirs(line: &str) -> V2 {
+fn parse_dirs(line: &str) -> (i32, i32) {
     let (_, dirs) = line.split_once(": ").unwrap();
     let (x_dir, y_dir) = dirs.split_once(", ").unwrap();
-    V2::new(toi32(&x_dir[2..]), toi32(&y_dir[2..]))
+    (toi32(&x_dir[2..]), toi32(&y_dir[2..]))
 }
 
 fn parse_machines(input: &str) -> Vec<Machine> {
@@ -37,10 +37,26 @@ fn parse_machines(input: &str) -> Vec<Machine> {
         .collect()
 }
 
-fn p1(input: &str) -> usize {
-    let machines = parse_machines(input);
-    println!("[DDA] day13::machines {:?}", machines);
-    10
+fn p1(input: &str) -> i32 {
+    let mut sum = 0;
+    for machine in parse_machines(input) {
+        let Machine {
+            btn_a,
+            btn_b,
+            prize,
+        } = machine;
+        let (ax, ay) = btn_a;
+        let (bx, by) = btn_b;
+        let (px, py) = prize;
+        let det = ax * by - ay * bx;
+        let nb_a = (by * px - bx * py) as f32 / det as f32;
+        let nb_b = (ax * py - ay * px) as f32 / det as f32;
+        if nb_a.fract() != 0.0 || nb_b.fract() != 0.0 || nb_a > 100.0 || nb_b > 100.0 {
+            continue;
+        }
+        sum += (nb_a as i32) * 3 + (nb_b as i32) * 1;
+    }
+    sum
 }
 
 //--------------------------------------------------------------------------------
@@ -53,8 +69,8 @@ fn p1(input: &str) -> usize {
 
 pub fn run() {
     pp_day("day13: Claw Contraption");
-    // time_it(p1, "p1", "data/13_input.txt");
-    time_it(p1, "p1", "data/13_sample.txt");
+    time_it(p1, "p1", "data/13_input.txt");
+    // time_it(p1, "p1", "data/13_sample.txt");
     // time_it(p2, "p2", "data/13_input.txt");
 }
 
@@ -64,7 +80,7 @@ mod tests {
 
     #[test]
     fn test() {
-        assert_eq!(run_it(p1, "data/13_sample.txt"), 140);
+        assert_eq!(run_it(p1, "data/13_sample.txt"), 480);
         // assert_eq!(run_it(p2, "data/13_sample2b.txt"), 368);
     }
 }
