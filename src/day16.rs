fb35c071add29cd102d760db796b275abca644c6
Didier Prophete <didier.prophete@gmail.com>
2024-12-16 01:51:52 -0800

    day 16 - part 2

--- src/day16.rs
+++ src/day16.rs
@@ -1,4 +1,7 @@
-use std::{collections::HashMap, convert::identity};
+use std::{
+    collections::{HashMap, HashSet},
+    convert::identity,
+};
 
 use crate::utils::*;
 
@@ -6,13 +9,16 @@ use crate::utils::*;
 // p1
 //--------------------------------------------------------------------------------
 
-fn p1(input: &str) -> i32 {
+fn score_per_pos(input: &str) -> (i32, HashMap<(V2, Dir), i32>) {
+    let mut min_score = i32::MAX;
     let mut matrix = Matrix::from_str(input, identity);
     let start = matrix.find_first('S').unwrap();
     let finish = matrix.find_first('E').unwrap();
     matrix.set(&start, '.');
     matrix.set(&finish, '.');
 
+    // we are going to keep the score at each position
+    // if we ever reach the same position with a higher score, we stop
     let mut visited = HashMap::new();
 
     let mut to_explore = vec![(start, Dir::Right, 0)];
@@ -22,13 +28,20 @@ fn p1(input: &str) -> i32 {
             continue;
         }
 
-        if let Some(prev_score) = visited.get(&pos) {
+        if let Some(prev_score) = visited.get(&(pos, dir)) {
             if score >= *prev_score {
                 // been there already with a lower score...
                 continue;
             }
         }
-        visited.insert(pos, score);
+        visited.insert((pos, dir), score);
+
+        if pos == finish {
+            if score < min_score {
+                min_score = score;
+            }
+            continue;
+        }
 
         let nx = pos.add(&dir.to_v2());
         to_explore.push((nx, dir, score + 1));
@@ -42,12 +55,68 @@ fn p1(input: &str) -> i32 {
         to_explore.push((nx_right, right_dir, score + 1 + 1000));
     }
 
-    *visited.get(&finish).unwrap()
+    (min_score, visited)
+}
+
+fn p1(input: &str) -> i32 {
+    let (score, _) = score_per_pos(input);
+    score
 }
 
 //--------------------------------------------------------------------------------
 // p2
 //--------------------------------------------------------------------------------
+
+fn p2(input: &str) -> usize {
+    let (p1_score, visited) = score_per_pos(input);
+
+    let mut matrix = Matrix::from_str(input, identity);
+    let start = matrix.find_first('S').unwrap();
+    let finish = matrix.find_first('E').unwrap();
+    matrix.set(&start, '.');
+    matrix.set(&finish, '.');
+
+    let mut all_seats = HashSet::new();
+
+    let mut to_explore = vec![(start, Dir::Right, 0, vec![])];
+    while let Some((pos, dir, score, path)) = to_explore.pop() {
+        if matrix.get(&pos) != Some('.') {
+            // we hit a wall
+            continue;
+        }
+
+        if let Some(prev_score) = visited.get(&(pos, dir)) {
+            if score > *prev_score {
+                continue;
+            }
+        }
+
+        if pos == finish {
+            if score == p1_score {
+                for seat in path {
+                    all_seats.insert(seat);
+                }
+                continue;
+            }
+        }
+
+        let mut path = path.clone();
+        path.push(pos);
+
+        let nx = pos.add(&dir.to_v2());
+        to_explore.push((nx, dir, score + 1, path.clone()));
+
+        let left_dir = dir.rot_left();
+        let nx_left = pos.add(&left_dir.to_v2());
+        to_explore.push((nx_left, left_dir, score + 1 + 1000, path.clone()));
+
+        let right_dir = dir.rot_right();
+        let nx_right = pos.add(&right_dir.to_v2());
+        to_explore.push((nx_right, right_dir, score + 1 + 1000, path));
+    }
+
+    1 + all_seats.len()
+}
 
 //--------------------------------------------------------------------------------
 // main
@@ -55,11 +124,12 @@ fn p1(input: &str) -> i32 {
 
 pub fn run() {
     pp_day("day16: Warehouse Woes");
-    time_it(p1, "p1", "data/16_sample.txt");
-    time_it(p1, "p1", "data/16_sample2.txt");
+    // time_it(p1, "p1", "data/16_sample.txt");
+    // time_it(p1, "p1", "data/16_sample2.txt");
     time_it(p1, "p1", "data/16_input.txt");
     // time_it(p2, "p2", "data/16_sample.txt");
-    // time_it(p2, "p2", "data/16_input.txt");
+    // time_it(p2, "p2", "data/16_sample2.txt");
+    time_it(p2, "p2", "data/16_input.txt");
 }
 
 #[cfg(test)]
@@ -70,5 +140,7 @@ mod tests {
     fn test() {
         assert_eq!(run_it(p1, "data/16_sample.txt"), 7036);
         assert_eq!(run_it(p1, "data/16_sample2.txt"), 11048);
+        assert_eq!(run_it(p2, "data/16_sample.txt"), 45);
+        assert_eq!(run_it(p2, "data/16_sample2.txt"), 64);
     }
 }
