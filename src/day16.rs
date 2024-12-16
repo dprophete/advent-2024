5361c92bfcc0d6a9b42c41735fb071670ae7e11f
Didier Prophete <didier.prophete@gmail.com>
2024-12-16 00:17:14 -0800

    part 1

--- src/day16.rs
+++ src/day16.rs
@@ -1,4 +1,4 @@
-use std::{collections::HashSet, convert::identity};
+use std::{collections::HashMap, convert::identity};
 
 use crate::utils::*;
 
@@ -13,36 +13,36 @@ fn p1(input: &str) -> i32 {
     matrix.set(&start, '.');
     matrix.set(&finish, '.');
 
-    let mut min_score = i32::MAX;
+    let mut visited = HashMap::new();
 
-    let mut visited = HashSet::new();
     let mut to_explore = vec![(start, Dir::Right, 0)];
     while let Some((pos, dir, score)) = to_explore.pop() {
-        if visited.contains(&pos) {
-            // been there already
-            continue;
-        }
         if matrix.get(&pos) != Some('.') {
             // we hit a wall
             continue;
         }
-        if pos == finish {
-            // we reached the end
-            if score < min_score {
-                min_score = score;
-            }
-            continue;
-        }
 
-        visited.insert(pos);
-        to_explore.push((pos.add(&dir.to_v2()), dir, score + 1));
+        if let Some(prev_score) = visited.get(&pos) {
+            if score >= *prev_score {
+                // been there already with a lower score...
+                continue;
+            }
+        }
+        visited.insert(pos, score);
+
+        let nx = pos.add(&dir.to_v2());
+        to_explore.push((nx, dir, score + 1));
+
         let left_dir = dir.rot_left();
+        let nx_left = pos.add(&left_dir.to_v2());
+        to_explore.push((nx_left, left_dir, score + 1 + 1000));
+
         let right_dir = dir.rot_right();
-        to_explore.push((pos.add(&left_dir.to_v2()), left_dir, score + 1 + 1000));
-        to_explore.push((pos.add(&right_dir.to_v2()), right_dir, score + 1 + 1000));
+        let nx_right = pos.add(&right_dir.to_v2());
+        to_explore.push((nx_right, right_dir, score + 1 + 1000));
     }
 
-    min_score
+    *visited.get(&finish).unwrap()
 }
 
 //--------------------------------------------------------------------------------
@@ -57,7 +57,7 @@ pub fn run() {
     pp_day("day16: Warehouse Woes");
     time_it(p1, "p1", "data/16_sample.txt");
     time_it(p1, "p1", "data/16_sample2.txt");
-    // time_it(p1, "p1", "data/16_input.txt");
+    time_it(p1, "p1", "data/16_input.txt");
     // time_it(p2, "p2", "data/16_sample.txt");
     // time_it(p2, "p2", "data/16_input.txt");
 }
