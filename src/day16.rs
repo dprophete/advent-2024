647877c7ef32c4e7582844540b1744285073465c
Didier Prophete <didier.prophete@gmail.com>
2024-12-17 17:50:47 -0800

    a little better

--- src/day16.rs
+++ src/day16.rs
@@ -43,15 +43,15 @@ fn score_per_pos(input: &str) -> (i32, HashMap<(V2, Dir), i32>) {
             continue;
         }
 
-        let nx = pos.add(dir.to_v2());
+        let nx = pos.add_dir(&dir);
         to_explore.push((nx, dir, score + 1));
 
         let dir_left = dir.rot_left();
-        let nx_left = pos.add(dir_left.to_v2());
+        let nx_left = pos.add_dir(&dir_left);
         to_explore.push((nx_left, dir_left, score + 1 + 1000));
 
         let dir_right = dir.rot_right();
-        let nx_right = pos.add(dir_right.to_v2());
+        let nx_right = pos.add_dir(&dir_right);
         to_explore.push((nx_right, dir_right, score + 1 + 1000));
     }
 
