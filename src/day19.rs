f1b60ed4eedabfa4a26a3613e6153de27db3bdad
Didier Prophete <didier.prophete@gmail.com>
2024-12-18 22:04:05 -0800

    day 19 - part 1

--- src/day19.rs
+++ src/day19.rs
@@ -21,13 +21,29 @@ impl Puzzle {
 
         Puzzle { patterns, designs }
     }
+
+    pub fn solve_for_design(&self, design: &Vec<char>) -> bool {
+        if design.is_empty() {
+            return true;
+        }
+        for p in &self.patterns {
+            if design.starts_with(p) {
+                if self.solve_for_design(&design[p.len()..].to_vec()) {
+                    return true;
+                }
+            }
+        }
+        false
+    }
 }
 
-fn p1(input: &str) -> usize {
+fn p1(input: &str) -> u32 {
     let puzzle = Puzzle::from_str(input);
-    println!("[DDA] day19::puzzle {:?}", puzzle);
-    // memory.nb_steps_to_escape()
-    10
+    let mut sum = 0;
+    for d in &puzzle.designs {
+        sum += bool_to_u32(puzzle.solve_for_design(&d));
+    }
+    sum
 }
 
 //--------------------------------------------------------------------------------
@@ -44,7 +60,7 @@ fn p1(input: &str) -> usize {
 pub fn run() {
     pp_day("day19: Linen Layout");
     time_it(p1, "p1", "data/19_sample.txt");
-    // time_it(p1, "p2", "data/19_input.txt");
+    time_it(p1, "p2", "data/19_input.txt");
     // time_it(p2, "p2", "data/19_sample.txt");
     // time_it(p2, "p2", "data/19_input.txt");
 }
@@ -56,6 +72,7 @@ mod tests {
     #[test]
     fn test() {
         assert_eq!(run_it(p1, "data/19_sample.txt"), 6);
+        assert_eq!(run_it(p1, "data/19_input.txt"), 242);
         // assert_eq!(run_it(p2, "data/19_sample.txt"), V2::new(6, 1));
         // assert_eq!(run_it(p2, "data/19_input.txt"), V2::new(28, 56));
     }
