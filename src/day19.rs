2a32e4d505e1f2681fd1bcebd4cc49ef08c5f96d
Didier Prophete <didier.prophete@gmail.com>
2024-12-18 22:07:19 -0800

    part 2 - setup

--- src/day19.rs
+++ src/day19.rs
@@ -22,18 +22,32 @@ impl Puzzle {
         Puzzle { patterns, designs }
     }
 
-    pub fn solve_for_design(&self, design: &Vec<char>) -> bool {
+    pub fn can_solve_for_design(&self, design: &Vec<char>) -> bool {
         if design.is_empty() {
             return true;
         }
         for p in &self.patterns {
             if design.starts_with(p) {
-                if self.solve_for_design(&design[p.len()..].to_vec()) {
+                if self.can_solve_for_design(&design[p.len()..].to_vec()) {
                     return true;
                 }
             }
         }
         false
+    }
+
+    pub fn all_solve_for_design(&self, design: &Vec<char>) -> u32 {
+        if design.is_empty() {
+            return 1;
+        }
+        // for p in &self.patterns {
+        //     if design.starts_with(p) {
+        //         if self.can_solve_for_design(&design[p.len()..].to_vec()) {
+        //             return true;
+        //         }
+        //     }
+        // }
+        0
     }
 }
 
@@ -41,7 +55,7 @@ fn p1(input: &str) -> u32 {
     let puzzle = Puzzle::from_str(input);
     let mut sum = 0;
     for d in &puzzle.designs {
-        sum += bool_to_u32(puzzle.solve_for_design(&d));
+        sum += puzzle.all_solve_for_design(&d);
     }
     sum
 }
@@ -50,8 +64,14 @@ fn p1(input: &str) -> u32 {
 // p2
 //--------------------------------------------------------------------------------
 
-// fn p2(input: &str) -> V2 {
-// }
+fn p2(input: &str) -> u32 {
+    let puzzle = Puzzle::from_str(input);
+    let mut sum = 0;
+    for d in &puzzle.designs {
+        sum += bool_to_u32(puzzle.can_solve_for_design(&d));
+    }
+    sum
+}
 
 //--------------------------------------------------------------------------------
 // main
@@ -60,8 +80,9 @@ fn p1(input: &str) -> u32 {
 pub fn run() {
     pp_day("day19: Linen Layout");
     time_it(p1, "p1", "data/19_sample.txt");
-    time_it(p1, "p2", "data/19_input.txt");
-    // time_it(p2, "p2", "data/19_sample.txt");
+    time_it(p1, "p1", "data/19_input.txt");
+    time_it(p2, "p2", "data/19_sample.txt");
+    // time_it(p1, "p2", "data/19_input.txt");
     // time_it(p2, "p2", "data/19_input.txt");
 }
 
@@ -73,7 +94,7 @@ mod tests {
     fn test() {
         assert_eq!(run_it(p1, "data/19_sample.txt"), 6);
         assert_eq!(run_it(p1, "data/19_input.txt"), 242);
-        // assert_eq!(run_it(p2, "data/19_sample.txt"), V2::new(6, 1));
+        assert_eq!(run_it(p2, "data/19_sample.txt"), 16);
         // assert_eq!(run_it(p2, "data/19_input.txt"), V2::new(28, 56));
     }
 }
