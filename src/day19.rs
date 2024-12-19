f4e47e04827132a335bf550003d24535ac6156cd
Didier Prophete <didier.prophete@gmail.com>
2024-12-18 22:42:41 -0800

    day 19 - part 2

--- src/day19.rs
+++ src/day19.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 use crate::utils::*;
 
 //--------------------------------------------------------------------------------
@@ -36,18 +38,26 @@ impl Puzzle {
         false
     }
 
-    pub fn all_solve_for_design(&self, design: &Vec<char>) -> u32 {
+    pub fn all_solve_for_design(
+        &self,
+        cache: &mut HashMap<Vec<char>, u64>,
+        design: &Vec<char>,
+    ) -> u64 {
         if design.is_empty() {
             return 1;
         }
-        // for p in &self.patterns {
-        //     if design.starts_with(p) {
-        //         if self.can_solve_for_design(&design[p.len()..].to_vec()) {
-        //             return true;
-        //         }
-        //     }
-        // }
-        0
+        match cache.get(design) {
+            Some(&v) => return v,
+            None => (),
+        }
+        let mut sum = 0;
+        for p in &self.patterns {
+            if design.starts_with(p) {
+                sum += self.all_solve_for_design(cache, &design[p.len()..].to_vec());
+            }
+        }
+        cache.insert(design.clone(), sum);
+        sum
     }
 }
 
@@ -55,7 +65,7 @@ fn p1(input: &str) -> u32 {
     let puzzle = Puzzle::from_str(input);
     let mut sum = 0;
     for d in &puzzle.designs {
-        sum += puzzle.all_solve_for_design(&d);
+        sum += bool_to_u32(puzzle.can_solve_for_design(&d));
     }
     sum
 }
@@ -64,11 +74,12 @@ fn p1(input: &str) -> u32 {
 // p2
 //--------------------------------------------------------------------------------
 
-fn p2(input: &str) -> u32 {
+fn p2(input: &str) -> u64 {
     let puzzle = Puzzle::from_str(input);
     let mut sum = 0;
     for d in &puzzle.designs {
-        sum += bool_to_u32(puzzle.can_solve_for_design(&d));
+        let mut cache: HashMap<Vec<char>, u64> = HashMap::new();
+        sum += puzzle.all_solve_for_design(&mut cache, &d);
     }
     sum
 }
@@ -82,8 +93,7 @@ pub fn run() {
     time_it(p1, "p1", "data/19_sample.txt");
     time_it(p1, "p1", "data/19_input.txt");
     time_it(p2, "p2", "data/19_sample.txt");
-    // time_it(p1, "p2", "data/19_input.txt");
-    // time_it(p2, "p2", "data/19_input.txt");
+    time_it(p2, "p2", "data/19_input.txt");
 }
 
 #[cfg(test)]
@@ -95,6 +105,6 @@ mod tests {
         assert_eq!(run_it(p1, "data/19_sample.txt"), 6);
         assert_eq!(run_it(p1, "data/19_input.txt"), 242);
         assert_eq!(run_it(p2, "data/19_sample.txt"), 16);
-        // assert_eq!(run_it(p2, "data/19_input.txt"), V2::new(28, 56));
+        assert_eq!(run_it(p2, "data/19_input.txt"), 595975512785325);
     }
 }
